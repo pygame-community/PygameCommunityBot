5dda7f347c20e95fa9705e08e015a52d381418c4
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2022-10-11 01:49:06 +0200

    Complete base implementation of text_command_manager bot extension

---
+++ pgbot/exts/text_command_manager/_types.py
@@ -0,0 +1,22 @@
+from typing import TypedDict
+from typing_extensions import NotRequired
+
+
+class GuildTextCommandState(TypedDict):
+
+    tcmd_uuid: str  # a UUID string of the command's ID
+    parent_tcmd_uuid: str  # a UUID string of the parent command's ID
+    qualified_name: str
+    enabled: int  # a 2 bit integer where 0b01 enables a command and 0b10 enables its subcommands
+    channels: NotRequired[
+        dict[int, bool]
+    ]  # a dictionary of channel IDs to booleans that marks
+    # in which channels a text command should be enabled in, or explicitly disabled
+    # (if e.g. enabled by a parent command). If not missing, this will add to or override a parent
+    # command's settings.
+    roles: NotRequired[
+        dict[int, bool]
+    ]  # a dictionary of role IDs to booleans that marks
+    # which roles enable the usage of a text command, or explicitly disables it
+    # (if e.g. enabled by a parent command). If not missing, this will add to or override a parent
+    # command's settings.
---
+++ pgbot/exts/text_command_manager/migrations.py
@@ -0,0 +1,30 @@
+from . import TABLE_NAME_PREFIX
+
+MIGRATIONS = {
+    "sqlite": {
+        "0.1.0": (
+            f"CREATE TABLE '{TABLE_NAME_PREFIX}guild_tcmd_states' ("
+            "guild_id INTEGER, "
+            "tcmd_uuid UUID, "
+            "parent_tcmd_uuid UUID NOT NULL, "
+            "qualified_name VARCHAR(100) NOT NULL, "
+            "enabled INTEGER NOT NULL, "
+            "channels BLOB, "
+            "roles BLOB, "
+            "PRIMARY KEY (guild_id, tcmd_uuid))"
+        )
+    },
+    "postgresql": {
+        "0.1.0": (
+            f"CREATE TABLE '{TABLE_NAME_PREFIX}guild_tcmd_states' ("
+            "guild_id BIGINT NOT NULL, "
+            "tcmd_uuid UUID NOT NULL, "
+            "parent_tcmd_uuid UUID NOT NULL, "
+            "qualified_name VARCHAR(100) NOT NULL, "
+            "enabled SMALLINT NOT NULL, "
+            "channels BYTEA, "
+            "roles BYTEA, "
+            "PRIMARY KEY (guild_id, tcmd_uuid))"
+        )
+    },
+}
