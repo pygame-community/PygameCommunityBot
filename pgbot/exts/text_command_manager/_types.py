82a4bdb29b36b3e5dfc114e16cc68a2efbda35a2
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2022-10-19 19:22:44 +0200

    improve _types.py for .text_command_manager/

--- pgbot/exts/text_command_manager/_types.py
+++ pgbot/exts/text_command_manager/_types.py
@@ -4,19 +4,28 @@ from typing_extensions import NotRequired
 
 class GuildTextCommandState(TypedDict):
 
-    tcmd_uuid: str  # a UUID string of the command's ID
-    parent_tcmd_uuid: str  # a UUID string of the parent command's ID
+    tcmd_uuid: str
+    "A UUID string representing the command's ID."
+    parent_tcmd_uuid: str
+    "A UUID string representing the parent command's ID."
     qualified_name: str
-    enabled: int  # a 2 bit integer where 0b01 enables a command and 0b10 enables its subcommands
-    channels: NotRequired[
-        dict[int, bool]
-    ]  # a dictionary of channel IDs to booleans that marks
-    # in which channels a text command should be enabled in, or explicitly disabled
-    # (if e.g. enabled by a parent command). If not missing, this will add to or override a parent
-    # command's settings.
-    roles: NotRequired[
-        dict[int, bool]
-    ]  # a dictionary of role IDs to booleans that marks
-    # which roles enable the usage of a text command, or explicitly disables it
-    # (if e.g. enabled by a parent command). If not missing, this will add to or override a parent
-    # command's settings.
+    "The qualified name of the text command."
+    enabled: int
+    "A 2 bit integer where ( 1 << 0 ) enables a command and ( 1 << 1 ) enables its "
+    "subcommands. If set to 0b01, all subcommands of a command will be recursively "
+    "disabled."
+    channels: NotRequired[dict[int, bool]]
+    """A dictionary of channel IDs to booleans that marks
+    in which channels a text command should be enabled in, or explicitly disabled
+    (if e.g. enabled by a parent command). If not missing, this will override or add "
+    "to a parent's setting, depending on whether the "All Channels" sentinel "
+    "(guild ID - 1)  is specified or not.
+    command's settings
+    """
+    roles: NotRequired[dict[int, bool]]
+    """A dictionary of role IDs to booleans that marks
+    which roles enable the usage of a text command, or explicitly disables it
+    (if e.g. enabled by a parent command). If not missing, this will override or add "
+    "to a parent's setting, depending on whether the "@everyone" role (guild ID) "
+    "is specified or not.
+    """
