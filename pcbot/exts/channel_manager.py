8c52a723145b666d6e2993401b5da0ff48a08042
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2024-06-01 15:40:56 +0200

    Add channel management extension with initial 'channel clone' command

---
+++ pcbot/exts/channel_manager.py
@@ -0,0 +1,131 @@
+"""This file is a part of the source code for PygameCommunityBot.
+
+Copyright (c) 2022-present pygame-community
+
+Bot extension for Discord channel management. 
+"""
+
+import discord
+from discord.ext import commands
+
+import snakecore
+from snakecore.commands.decorators import flagconverter_kwargs
+from snakecore.commands.converters import CodeBlock, String, Parens, TimeDelta
+
+from ..base import BaseExtensionCog
+
+BotT = snakecore.commands.Bot | snakecore.commands.AutoShardedBot
+
+
+async def clone_forum(
+    forum: discord.ForumChannel, new_name: str | None = None, reason: str | None = None
+) -> discord.ForumChannel:
+    return await forum.guild.create_forum(
+        name=new_name if new_name else forum.name,
+        topic=forum.topic or discord.utils.MISSING,
+        category=forum.category,
+        position=forum.position + 1,
+        slowmode_delay=forum.slowmode_delay,
+        overwrites=(
+            discord.utils.MISSING if forum.permissions_synced else forum.overwrites
+        ),  # type: ignore
+        default_auto_archive_duration=forum.default_auto_archive_duration,
+        default_thread_slowmode_delay=forum.default_thread_slowmode_delay,
+        default_sort_order=(
+            forum.default_sort_order
+            if forum.default_sort_order is not None
+            else discord.utils.MISSING
+        ),
+        default_reaction_emoji=(
+            forum.default_reaction_emoji
+            if forum.default_reaction_emoji is not None
+            else discord.utils.MISSING
+        ),
+        default_layout=forum.default_layout,
+        available_tags=(
+            forum.available_tags if forum.available_tags else discord.utils.MISSING
+        ),
+        reason=reason,
+    )
+
+
+async def clone_category(
+    category: discord.CategoryChannel,
+    new_name: str | None,
+    clone_channels: bool = True,
+    reason: str | None = None,
+):
+    new_category = await category.clone(name=new_name)
+
+    if clone_channels:
+        for channel in category.channels:
+            if isinstance(channel, discord.ForumChannel):
+                channel_clone = await clone_forum(
+                    channel, new_name=new_name, reason=reason
+                )
+            else:
+                channel_clone = await channel.clone()
+
+            await channel_clone.move(category=new_category)  # type: ignore
+
+    return new_category
+
+
+class ChannelManagerCog(BaseExtensionCog, name="channels"):
+    """Channel management commands."""
+
+    @commands.group(invoke_without_command=True)
+    async def channel(self, ctx: commands.Context[BotT]):
+        pass
+
+    @channel.command(
+        usage="<channel: Channel> [new_name: Text[100]] [deep_clone_category: yes|no]",
+    )
+    @flagconverter_kwargs()
+    async def channel_clone(
+        self,
+        ctx: commands.Context[BotT],
+        channel: (
+            discord.TextChannel
+            | discord.VoiceChannel
+            | discord.ForumChannel
+            | discord.StageChannel
+            | discord.CategoryChannel
+        ),
+        new_name: str | None = None,
+        deep_clone_category: bool = False,
+    ):
+        """Clone the specified channel. More reliable than Discord's built-in channel cloning system.
+
+        __**Parameters:**__
+
+        **`<channel: Channel>`**
+        > The channel to clone.
+
+        **`[new_name: Text[100]]`**
+        > The new name of the cloned channel. Must not exceed 100 characters.
+
+        **`[deep_clone_category: yes|no]`**
+        > Whether to clone all channels within a specified category channel.
+        > Defaults to 'no'.
+        """
+        if isinstance(channel, discord.CategoryChannel):
+            clone = await clone_category(
+                channel,
+                new_name=new_name,
+                clone_channels=deep_clone_category,
+                reason=f"Channel {channel.name} ({channel.mention}) cloned as "
+                f"{clone.name} ({clone.mention}) upon request by "  # type: ignore
+                f"{ctx.author.name} ({ctx.author.mention})",
+            )
+        elif isinstance(channel, discord.ForumChannel):
+            clone = await clone_forum(
+                channel,
+                reason=f"Channel created as clone of #{channel.name} "
+                f"({channel.mention}) upon request by {ctx.author.name} "
+                f"({ctx.author.mention})",
+            )
+
+
+async def setup(bot: BotT, color: int | discord.Color = 0):
+    await bot.add_cog(ChannelManagerCog(bot, theme_color=color))
