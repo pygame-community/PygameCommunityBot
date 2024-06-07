1e43ad0cf1d247a67d9ed30b367c6fdea0fadabf
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2024-06-07 02:01:55 +0200

    Refactor 'op' extension

--- pcbot/exts/op.py
+++ pcbot/exts/op.py
@@ -9,7 +9,7 @@ from discord import app_commands
 from discord.ext import commands
 import snakecore
 from snakecore.commands import flagconverter_kwargs
-from snakecore.commands import ReferencedMessage
+from snakecore.commands import ReferencedMessage, Message
 
 from ..base import BaseExtensionCog
 
@@ -19,15 +19,129 @@ MessageableGuildChannel = (
 )
 
 
+invocation_error = lambda ctx, *args: (
+    app_commands.CommandInvokeError(
+        ctx.interaction.command, app_commands.AppCommandError(*args)
+    )
+    if ctx.interaction
+    else commands.CommandInvokeError(commands.CommandError(*args))
+)
+
+
 class OPCog(BaseExtensionCog, name="op"):
     """Commands related to being an OP (original poster) in a forum post or thread."""
 
     @commands.guild_only()
-    @commands.hybrid_group()
+    @commands.group(invoke_without_command=True)
     async def op(self, ctx: commands.Context[BotT]):
         pass
 
-    @commands.guild_only()
+    op_app_cmd_group = app_commands.Group(
+        name="op",
+        description="Commands related to being an OP (original poster) in a forum post or thread.",
+        guild_only=True,
+    )
+
+    async def op_pin_func(
+        self,
+        ctx: commands.Context[BotT],
+        message: discord.Message,
+        unpin_last: bool = False,
+        delete_system_message: bool = False,
+    ):
+        assert (
+            ctx.guild
+            and ctx.bot.user
+            and (bot_member := ctx.guild.get_member(ctx.bot.user.id))
+            and isinstance(
+                ctx.channel,
+                (discord.TextChannel, discord.VoiceChannel, discord.Thread),
+            )
+            and isinstance(ctx.author, discord.Member)
+        )
+
+        channel = ctx.channel
+
+        if not isinstance(channel, discord.Thread):
+            raise (
+                invocation_error(
+                    ctx,
+                    "This command does not work outside of threads and posts.",  # type: ignore
+                )
+            )
+
+        elif not (
+            (channel.owner_id == ctx.author.id)
+            or snakecore.utils.have_permissions_in_channels(
+                ctx.author,
+                channel,
+                "view_channel",
+                "manage_messages",
+            )
+        ):
+            raise (
+                invocation_error(
+                    ctx,
+                    "You do not have enough permissions to run this command in this "
+                    f"channel (<#{channel.id}>).",  # type: ignore
+                )
+            )
+
+        elif not message.channel.id == channel.id:
+            raise (
+                invocation_error(
+                    ctx,
+                    "The specified message must originate from this channel "
+                    f"(<#{channel.id}>).",
+                )
+            )
+
+        if message.pinned:
+            raise (
+                invocation_error(ctx, f"Message {message.jump_url} is already pinned.")
+            )
+
+        to_unpin = None
+
+        if unpin_last:
+            if len((pins := await channel.pins())) == 50:
+                to_unpin = pins[0]
+                await to_unpin.unpin()
+
+        await message.pin()
+
+        if delete_system_message:
+            try:
+                system_message: discord.Message = await ctx.bot.wait_for(
+                    "message",
+                    check=(
+                        lambda m: m.channel.id == channel.id
+                        and m.type == discord.MessageType.pins_add
+                    ),
+                    timeout=2,
+                )
+                print("HERE")
+            except asyncio.TimeoutError:
+                pass
+            else:
+                await system_message.delete()
+
+        success_embed = discord.Embed(
+            description=f"Successfully pinned {message.jump_url}."
+            + (f"\n{to_unpin.jump_url} was unpinned." if to_unpin else ""),
+            color=self.theme_color,
+        )
+
+        if ctx.interaction:
+            await ctx.interaction.response.send_message(
+                embed=success_embed, ephemeral=True
+            )
+        else:
+            await self.send_or_edit_response(
+                ctx,
+                embed=success_embed,
+            )
+
     @op.command(
         name="pin",
         usage="<message: Message> [delete_system_message: yes|no] [unpin_last: yes|no]",
@@ -36,19 +150,13 @@ class OPCog(BaseExtensionCog, name="op"):
             response_deletion_with_reaction=True,
         ),
     )
-    @app_commands.describe(
-        message="The message to pin.",
-        unpin_last="Whether to unpin the least recently pinned message(s) if the "
-        "maximum pin limit of 50 would be reached.",
-        delete_system_message="whether any pin system messages should be automatically"
-        " deleted. Defaults to False.",
-    )
     @flagconverter_kwargs()
     async def op_pin(
         self,
         ctx: commands.Context[BotT],
+        message_: ReferencedMessage | discord.Message,
         *,
-        message: discord.PartialMessage | ReferencedMessage,
+        message: discord.Message | None = None,
         unpin_last: bool = False,
         delete_system_message: bool = False,
     ):
@@ -72,124 +180,51 @@ class OPCog(BaseExtensionCog, name="op"):
         > limit of 50 would be reached.
         > Defaults to 'no'.
         """
-        assert (
-            ctx.guild
-            and ctx.bot.user
-            and (bot_member := ctx.guild.get_member(ctx.bot.user.id))
-            and isinstance(
-                ctx.channel,
-                (discord.TextChannel, discord.VoiceChannel, discord.Thread),
+
+        if not (message := message_ or message):
+            raise commands.CommandInvokeError(
+                commands.BadArgument(
+                    "'message' is a required argument that is missing."
+                )
             )
-            and isinstance(ctx.author, discord.Member)
+
+        return await self.op_pin_func(
+            ctx,
+            message=message,
+            unpin_last=unpin_last,
+            delete_system_message=delete_system_message,
         )
 
-        channel = ctx.channel
-
-        if not isinstance(channel, discord.Thread):
-            raise commands.CommandInvokeError(
-                commands.CommandError(
-                    "This command does not work outside of threads and posts."  # type: ignore
-                )
-            )
-
-        elif not (
-            (channel.owner_id == ctx.author.id)
-            or snakecore.utils.have_permissions_in_channels(
-                ctx.author,
-                channel,
-                "view_channel",
-                "manage_messages",
-            )
-        ):
-            raise commands.CommandInvokeError(
-                commands.CommandError(
-                    "You do not have enough permissions to run this command in this "
-                    f"channel (<#{channel.id}>)."  # type: ignore
-                )
-            )
-
-        elif not message.id == channel.id:
-            raise commands.CommandInvokeError(
-                commands.CommandError(
-                    "The specified message must originate from this channel "
-                    f"(<#{channel.id}>).",
-                )
-            )
-
-        if message.pinned:
-            raise commands.CommandInvokeError(
-                commands.CommandError(f"Message {message.jump_url} is already pinned.")
-            )
-
-        to_unpin = None
-
-        if unpin_last:
-            if len((pins := await channel.pins())) == 50:
-                to_unpin = pins[0]
-                await to_unpin.unpin()
-
-        await message.pin()
-
-        if delete_system_message:
-            try:
-                system_message: discord.Message = await ctx.bot.wait_for(
-                    "message",
-                    check=(
-                        lambda m: m.channel.id == channel.id  # type: ignore
-                        and m.type == discord.MessageType.pins_add
-                    ),
-                    timeout=2,
-                )
-            except asyncio.TimeoutError:
-                pass
-            else:
-                await system_message.delete()
-
-        success_embed = discord.Embed(
-            description=f"Successfully pinned {message.jump_url}."
-            + (f"\n{to_unpin.jump_url} was unpinned." if to_unpin else ""),
-            color=self.theme_color,
-        )
-
-        if ctx.interaction:
-            await ctx.interaction.response.send_message(
-                embed=success_embed, ephemeral=True
-            )
-        else:
-            await self.send_or_edit_response(
-                ctx,
-                embed=success_embed,
-            )
-
-    @commands.guild_only()
-    @op.command(
-        name="unpin",
-        usage="<message: Message>",
-        description="Unpin the specified message.",
-        extras=dict(
-            response_deletion_with_reaction=True,
-        ),
+    @op_app_cmd_group.command(
+        name="pin",
+        description="Pin the specified message.",
     )
     @app_commands.describe(
-        message="The message to unpin.",
+        message="The message to pin.",
+        unpin_last="Whether to unpin the least recently pinned message(s) if the "
+        "maximum pin limit of 50 would be reached.",
+        delete_system_message="whether any pin system messages should be automatically"
+        " deleted. Defaults to False.",
     )
-    @flagconverter_kwargs()
-    async def op_unpin(
+    async def op_pin_app_cmd(
+        self,
+        interaction: discord.Interaction[BotT],
+        message: Message,
+        unpin_last: bool = False,
+        delete_system_message: bool = False,
+    ):
+        return await self.op_pin_func(
+            await commands.Context[BotT].from_interaction(interaction),
+            message=await message.channel.fetch_message(message.id),
+            unpin_last=unpin_last,
+            delete_system_message=delete_system_message,
+        )
+
+    async def op_unpin_func(
         self,
         ctx: commands.Context[BotT],
-        *,
-        message: discord.Message | ReferencedMessage,
+        message: discord.Message,
     ):
-        """Unpin the specified message.
-
-        In threads, forum posts, or text channels with "OP: <@{user_id}> ..." people will always be able to use this command to
-        pin messages as the OP (original poster).
-
-        __**Parameters:**__
-
-        **`<message: Message>`**
-        > The message to unpin.
-        """
         assert (
             ctx.guild
             and ctx.bot.user
@@ -204,9 +239,10 @@ class OPCog(BaseExtensionCog, name="op"):
         channel = ctx.channel
 
         if not isinstance(channel, discord.Thread):
-            raise commands.CommandInvokeError(
-                commands.CommandError(
-                    "This command does not work outside of threads and posts."  # type: ignore
+            raise (
+                invocation_error(
+                    ctx,
+                    "This command does not work outside of threads and posts.",
                 )
             )
 
@@ -219,25 +255,25 @@ class OPCog(BaseExtensionCog, name="op"):
                 "manage_messages",
             )
         ):
-            raise commands.CommandInvokeError(
-                commands.CommandError(
+            raise (
+                invocation_error(
+                    ctx,
                     "You do not have enough permissions to run this command in this "
-                    f"channel (<#{channel.id}>)."  # type: ignore
+                    f"channel (<#{channel.id}>).",
                 )
             )
 
-        elif not message.id == channel.id:
-            raise commands.CommandInvokeError(
-                commands.CommandError(
+        elif not message.channel.id == channel.id:
+            raise (
+                invocation_error(
+                    ctx,
                     "The specified message must originate from this channel "
                     f"(<#{channel.id}>).",
                 )
             )
 
         if not message.pinned:
-            raise commands.CommandInvokeError(
-                commands.CommandError(f"Message {message.jump_url} is not pinned.")
-            )
+            raise (invocation_error(ctx, f"Message {message.jump_url} is not pinned."))
 
         await message.unpin()
 
@@ -254,7 +290,58 @@ class OPCog(BaseExtensionCog, name="op"):
                 embed=success_embed,
             )
 
-    # add event listeners, commands, state, etc.
+    @commands.guild_only()
+    @op.command(
+        name="unpin",
+        usage="<message: Message>",
+        extras=dict(
+            response_deletion_with_reaction=True,
+        ),
+    )
+    @flagconverter_kwargs()
+    async def op_unpin(
+        self,
+        ctx: commands.Context[BotT],
+        message_: discord.Message | ReferencedMessage | None,
+        *,
+        message: discord.Message | None = None,
+    ):
+        """Unpin the specified message.
+
+        In threads, forum posts, or text channels with "OP: <@{user_id}> ..." people will always be able to use this command to
+        pin messages as the OP (original poster).
+
+        __**Parameters:**__
+
+        **`<message: Message>`**
+        > The message to unpin.
+        """
+
+        if not (message := message_ or message):
+            raise commands.CommandInvokeError(
+                commands.BadArgument(
+                    "'message' is a required argument that is missing."
+                )
+            )
+        return await self.op_unpin_func(ctx, message=message)
+
+    @op_app_cmd_group.command(
+        name="unpin",
+        description="Unpin the specified message.",
+    )
+    @app_commands.describe(
+        message="The message to unpin.",
+    )
+    async def op_unpin_app_cmd(
+        self,
+        interaction: discord.Interaction[BotT],
+        message: Message,
+    ):
+        return await self.op_unpin_func(
+            await commands.Context[BotT].from_interaction(interaction), message=message
+        )
+
+    op_unpin_app_cmd.callback.__doc__ = op_unpin.usage
 
 
 @snakecore.commands.decorators.with_config_kwargs
