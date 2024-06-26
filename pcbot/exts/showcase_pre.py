9e04aaa58f7a8589cac34f02da4d4677593f8151
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2024-06-26 15:14:57 +0200

    Refactor 'showcase_pre' extension to only support forum channel based showcase channels

--- pcbot/exts/showcase_pre.py
+++ pcbot/exts/showcase_pre.py
@@ -16,20 +16,20 @@ from ..base import BaseExtensionCog
 
 BotT = snakecore.commands.Bot | snakecore.commands.AutoShardedBot
 
-SHOWCASE_ENTRIES_CHANNEL = 772507247540437032
-ENTRIES_DISCUSSION_CHANNEL = 780351772514058291
-
 
 class ShowcasePreCog(BaseExtensionCog, name="showcase-pre"):
-    def __init__(self, bot: BotT, theme_color: int | discord.Color = 0) -> None:
+    def __init__(
+        self, bot: BotT, showcase_channel_id: int, theme_color: int | discord.Color = 0
+    ) -> None:
         super().__init__(bot, theme_color=theme_color)
-        self.entry_message_deletion_dict: dict[int, tuple[asyncio.Task[None], int]] = {}
+        self.showcase_channel_id = showcase_channel_id
+        self.entry_post_deletion_dict: dict[int, tuple[asyncio.Task[None], int]] = {}
 
     @staticmethod
     async def delete_bad_entry_and_warning(
         entry_msg: discord.Message, warn_msg: discord.Message, delay: float = 0.0
     ):
-        """A function to pardon a bad entry message with a grace period. If this coroutine is not cancelled during the
+        """A function to pardon a bad entry post with a grace period. If this coroutine is not cancelled during the
         grace period specified in `delay` in seconds, it will delete both `entry_msg` and `warn_msg`, if possible.
         """
         try:
@@ -46,56 +46,15 @@ class ShowcasePreCog(BaseExtensionCog, name="showcase-pre"):
                     pass
 
     @staticmethod
-    def format_entries_message(
-        msg: discord.Message,
-    ) -> tuple[str, list[dict[str, str | bool]]]:
-        """
-        Formats an entries message to be reposted in discussion channel
-        """
-        f"New showcase in #\u200b{msg.channel.name}"  # type: ignore
-        title = ""
-
-        attachments = ""
-        if msg.attachments:
-            for i, attachment in enumerate(msg.attachments):
-                if (
-                    len(attachments)
-                    + len((attr_str := f" • [Link {i + 1}]({attachment.url})\n"))
-                    < 1024
-                ):
-                    attachments += attr_str
-                else:
-                    break
-        else:
-            attachments = "No attachments"
-
-        desc = msg.content if msg.content else "No description provided."
-
-        fields = [
-            {"name": "**Posted by**", "value": msg.author.mention, "inline": True},
-            {
-                "name": "**Original msg.**",
-                "value": f"[View]({msg.jump_url})",
-                "inline": True,
-            },
-            {"name": "**Attachments**", "value": attachments, "inline": True},
-            {
-                "name": "**Description**",
-                "value": desc if len(desc) < 1024 else desc[:1022] + "...",
-                "inline": True,
-            },
-        ]
-        return title, fields
-
-    @staticmethod
     def entry_message_validity_check(
         message: discord.Message, min_chars=32, max_chars=float("inf")
     ):
-        """Checks if a message posted in a showcase channel for projects has the right format.
+        """Checks if a post's starter message posted in a showcase channel has the right format.
 
-            Returns
+        Returns
         -------
-                bool: True/False
+        bool:
+            True/False
         """
         search_obj = re.search(
             snakecore.utils.regex_patterns.URL, message.content or ""
@@ -116,74 +75,68 @@ class ShowcasePreCog(BaseExtensionCog, name="showcase-pre"):
         return False
 
     @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        if message.channel.id != SHOWCASE_ENTRIES_CHANNEL or message.author.bot:
+    async def on_thread_create(self, thread: discord.Thread):
+        if thread.parent_id != self.showcase_channel_id:
             return
 
-        assert message.guild
+        try:
+            message = thread.starter_message or await thread.fetch_message(thread.id)
+        except discord.NotFound:
+            pass
+        else:
+            await message.pin()
+
         if not self.entry_message_validity_check(message):
             deletion_datetime = datetime.datetime.now(
                 datetime.timezone.utc
             ) + datetime.timedelta(minutes=2)
             warn_msg = await message.reply(
-                "Your entry message must contain an attachment or text and safe links to be valid.\n\n"
+                "Your post must contain an attachment or text and safe links to be valid.\n\n"
                 "- Attachment-only entries must be replies to a previous entry of yours.\n"
                 "- Text-only entries must contain at least 32 characters (including links, but not links alone).\n\n"
-                "If you meant to comment on another entry, please delete your"
-                f" message and go to <#{ENTRIES_DISCUSSION_CHANNEL}> .\n\n"
-                " If no changes are made, your entry message will be"
+                " If no changes are made, your post will be"
                 f" deleted {snakecore.utils.create_markdown_timestamp(deletion_datetime, 'R')}."
             )
-            self.entry_message_deletion_dict[message.id] = [  # type: ignore
+            self.entry_post_deletion_dict[thread.id] = (
                 asyncio.create_task(
                     self.delete_bad_entry_and_warning(message, warn_msg, delay=120)
                 ),
                 warn_msg.id,
-            ]
-            return
-
-        color = 0xFF8800
-        title, fields = self.format_entries_message(message)
-
-        entries_discussion_channel = message.guild.get_channel(
-            ENTRIES_DISCUSSION_CHANNEL
-        ) or await message.guild.fetch_channel(ENTRIES_DISCUSSION_CHANNEL)
-
-        await entries_discussion_channel.send(  # type: ignore
-            embed=discord.Embed.from_dict(dict(title=title, color=color, fields=fields))
-        )
+            )
 
     @commands.Cog.listener()
     async def on_message_edit(self, old: discord.Message, new: discord.Message):
-        if new.channel.id != SHOWCASE_ENTRIES_CHANNEL or new.author.bot:
+        if not (
+            isinstance(new.channel, discord.Thread)
+            and new.channel.parent_id == self.showcase_channel_id
+        ):
             return
-        embed_repost_edited = False
+
+        thread = new.channel
+
         if not self.entry_message_validity_check(new):
-            if new.id in self.entry_message_deletion_dict:
-                deletion_data_tuple = self.entry_message_deletion_dict[new.id]
+            if thread.id in self.entry_post_deletion_dict:
+                deletion_data_tuple = self.entry_post_deletion_dict[thread.id]
                 deletion_task = deletion_data_tuple[0]
                 if deletion_task.done():
-                    del self.entry_message_deletion_dict[new.id]
+                    del self.entry_post_deletion_dict[thread.id]
                 else:
                     try:
                         deletion_task.cancel()  # try to cancel deletion after noticing edit by sender
-                        warn_msg = await new.channel.fetch_message(
-                            deletion_data_tuple[1]
-                        )
+                        warn_msg = await thread.fetch_message(deletion_data_tuple[1])
                         deletion_datetime = datetime.datetime.now(
                             datetime.timezone.utc
                         ) + datetime.timedelta(minutes=2)
                         await warn_msg.edit(
                             content=(
-                                "I noticed your edit, but: Your entry message must contain an attachment or a (Discord recognized) link to be valid."
+                                "I noticed your edit, but: Your post must contain an attachment or a (Discord recognized) link to be valid."
                                 " If it doesn't contain any characters but an attachment, it must be a reply to another entry you created."
                                 f" If no attachments are present, it must contain at least 32 characters (including any links, but not links alone)."
-                                f" If you meant to comment on another entry, please delete your message and go to <#{ENTRIES_DISCUSSION_CHANNEL}> ."
-                                " If no changes are made, your entry message will be"
+                                " If no changes are made, your post will be"
                                 f" deleted {snakecore.utils.create_markdown_timestamp(deletion_datetime, 'R')}."
                             )
                         )
-                        self.entry_message_deletion_dict[new.id] = (
+                        self.entry_post_deletion_dict[thread.id] = (
                             asyncio.create_task(
                                 self.delete_bad_entry_and_warning(
                                     new, warn_msg, delay=120
@@ -193,24 +146,23 @@ class ShowcasePreCog(BaseExtensionCog, name="showcase-pre"):
                         )
                     except (
                         discord.NotFound
-                    ):  # cancelling didn't work, warning and entry message were already deleted
-                        if new.id in self.entry_message_deletion_dict:
-                            del self.entry_message_deletion_dict[new.id]
+                    ):  # cancelling didn't work, warning and post were already deleted
+                        if thread.id in self.entry_post_deletion_dict:
+                            del self.entry_post_deletion_dict[thread.id]
 
-            else:  # an edit led to an invalid entry message from a valid one
+            else:  # an edit led to an invalid post from a valid one
                 deletion_datetime = datetime.datetime.now(
                     datetime.timezone.utc
                 ) + datetime.timedelta(minutes=2)
                 warn_msg = await new.reply(
-                    "Your entry message must contain an attachment or text and a (Discord recognized) link to be valid."
+                    "Your post must contain an attachment or text and a (Discord recognized) link to be valid."
                     " If it doesn't contain any characters but an attachment, it must be a reply to another entry you created."
                     f" If no attachments are present, it must contain at least 32 characters (including any links, but not links alone)."
-                    f" If you meant to comment on another entry, please delete your message and go to <#{ENTRIES_DISCUSSION_CHANNEL}> ."
-                    " If no changes are made, your entry message will be"
+                    " If no changes are made, your post will be"
                     f" deleted {snakecore.utils.create_markdown_timestamp(deletion_datetime, 'R')}."
                 )
 
-                self.entry_message_deletion_dict[new.id] = (
+                self.entry_post_deletion_dict[thread.id] = (
                     asyncio.create_task(
                         self.delete_bad_entry_and_warning(new, warn_msg, delay=120)
                     ),
@@ -220,115 +172,55 @@ class ShowcasePreCog(BaseExtensionCog, name="showcase-pre"):
 
         elif (
             self.entry_message_validity_check(new)
-            and new.id in self.entry_message_deletion_dict
+            and thread.id in self.entry_post_deletion_dict
         ):  # an invalid entry was corrected
-            deletion_data_tuple = self.entry_message_deletion_dict[new.id]
+            deletion_data_tuple = self.entry_post_deletion_dict[thread.id]
             deletion_task = deletion_data_tuple[0]
             if not deletion_task.done():  # too late to do anything
                 try:
                     deletion_task.cancel()  # try to cancel deletion after noticing valid edit by sender
-                    warn_msg = await new.channel.fetch_message(deletion_data_tuple[1])
+                    warn_msg = await thread.fetch_message(deletion_data_tuple[1])
                     await warn_msg.delete()
                 except (
                     discord.NotFound
-                ):  # cancelling didn't work, warning and entry message were already deleted
+                ):  # cancelling didn't work, warning and post were already deleted
                     pass
 
-            if new.id in self.entry_message_deletion_dict:
-                del self.entry_message_deletion_dict[new.id]
-
-        entries_discussion_channel = new.guild.get_channel(  # type: ignore
-            ENTRIES_DISCUSSION_CHANNEL
-        ) or await new.guild.fetch_channel(  # type: ignore
-            ENTRIES_DISCUSSION_CHANNEL
-        )
-
-        async for message in entries_discussion_channel.history(  # type: ignore # attempt to find and edit repost
-            around=old.created_at, limit=5
-        ):
-            if not (message.author.id == self.bot.user.id and (embeds := message.embeds)):  # type: ignore
-                continue
-            try:
-                embed = embeds[0]
-                if not embed.fields:
-                    continue
-                link = embed.fields[1].value
-                if not isinstance(link, str):
-                    continue
-
-                if int(link.split("/")[6][:-1]) == new.id:
-                    _, fields = self.format_entries_message(new)
-                    await message.edit(
-                        embed=discord.Embed.from_dict(
-                            embed.to_dict() | {"fields": fields}
-                        )
-                    )
-                    embed_repost_edited = True
-                    break
-            except (IndexError, AttributeError):
-                pass
-
-        if not embed_repost_edited:
-            if (
-                datetime.datetime.now(datetime.timezone.utc) - old.created_at
-            ) < datetime.timedelta(
-                minutes=5
-            ):  # for new, recently corrected entry messages
-                color = 0xFF8800
-
-                title, fields = self.format_entries_message(new)
-                await entries_discussion_channel.send(  # type: ignore
-                    embed=discord.Embed.from_dict(
-                        dict(title=title, color=color, fields=fields)
-                    )
-                )
+            if thread.id in self.entry_post_deletion_dict:
+                del self.entry_post_deletion_dict[thread.id]
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message):
-        if (
-            message.channel.id in (SHOWCASE_ENTRIES_CHANNEL, ENTRIES_DISCUSSION_CHANNEL)
-            and message.channel
-            and message.guild
+
+        if not (
+            isinstance(message.channel, discord.Thread)
+            and message.channel.parent_id == self.showcase_channel_id
         ):
-            if (
-                message.channel.id == SHOWCASE_ENTRIES_CHANNEL
-                and message.id in self.entry_message_deletion_dict
-            ):  # for case where user deletes their bad entry by themselves
-                deletion_data_list = self.entry_message_deletion_dict[message.id]
-                deletion_task = deletion_data_list[0]
-                if not deletion_task.done():
-                    deletion_task.cancel()
-                    try:
-                        warn_msg = await message.channel.fetch_message(
-                            deletion_data_list[1]
-                        )  # warning and entry message were already deleted
-                        await warn_msg.delete()
-                    except discord.NotFound:
-                        pass
+            return
 
-                if message.id in self.entry_message_deletion_dict:
-                    del self.entry_message_deletion_dict[message.id]
+        thread = message.channel
 
-            entries_discussion_channel = message.guild.get_channel(
-                ENTRIES_DISCUSSION_CHANNEL
-            ) or await message.guild.fetch_channel(ENTRIES_DISCUSSION_CHANNEL)
-
-            async for message in entries_discussion_channel.history(  # type: ignore
-                around=message.created_at, limit=5
-            ):
+        if (
+            thread.id in self.entry_post_deletion_dict
+        ):  # for case where user deletes their bad entry by themselves
+            deletion_data_tuple = self.entry_post_deletion_dict[thread.id]
+            deletion_task = deletion_data_tuple[0]
+            if not deletion_task.done():
+                deletion_task.cancel()
                 try:
-                    link = message.embeds[0].fields[1].value
-                    if not isinstance(link, str):
-                        continue
-
-                    if int(link.split("/")[6][:-1]) == message.id:
-                        await message.delete()
-                        break
-
-                except (IndexError, AttributeError):
+                    warn_msg = await message.channel.fetch_message(
+                        deletion_data_tuple[1]
+                    )  # warning and post were already deleted
+                    await warn_msg.delete()
+                except discord.NotFound:
                     pass
+
+            if thread.id in self.entry_post_deletion_dict:
+                del self.entry_post_deletion_dict[thread.id]
 
 
 @snakecore.commands.decorators.with_config_kwargs
-async def setup(bot: BotT):
-    await bot.add_cog(ShowcasePreCog(bot))
+async def setup(
+    bot: BotT, showcase_channel_id: int, theme_color: int | discord.Color = 0
+):
+    await bot.add_cog(ShowcasePreCog(bot, showcase_channel_id, theme_color))
