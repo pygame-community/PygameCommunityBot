ee17a7dadff964973da6826b25c3564a88fe9296
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2024-07-26 05:16:59 +0200

    Refactor 'anti-crosspost' cog in 'anti_crosspost' extension (take 2)

--- pcbot/exts/anti_crosspost.py
+++ pcbot/exts/anti_crosspost.py
@@ -24,7 +24,7 @@ def crosspost_cmp(message: discord.Message, other: discord.Message) -> bool:
         other (discord.Message): The second message to compare.
 
     Returns:
-        bool: True if the messages are similar enough to be considered 
+        bool: True if the messages are similar enough to be considered
         duplicates, otherwise False.
     """
     if not message.content or not other.content:
@@ -54,6 +54,7 @@ class AntiCrosspostCog(BaseExtensionCog, name="anti-crosspost"):
         self,
         bot: BotT,
         channel_ids: Collection[int],
+        crosspost_timedelta_threshold: int,
         message_length_threshold: int,
         max_tracked_users: int,
         max_tracked_message_groups_per_user: int,
@@ -65,6 +66,7 @@ class AntiCrosspostCog(BaseExtensionCog, name="anti-crosspost"):
         Args:
             bot (BotT): The bot instance.
             channel_ids (Collection[int]): Collection of channel IDs to monitor.
+            crosspost_timedelta_threshold (int): Minimum time difference between messages to not be considered crossposts.
             message_length_threshold (int): Minimum length of a message to be considered.
             max_tracked_users (int): Maximum number of users to track.
             max_tracked_message_groups_per_user (int): Maximum number of message
@@ -75,6 +77,7 @@ class AntiCrosspostCog(BaseExtensionCog, name="anti-crosspost"):
         self.channel_ids = set(channel_ids)
         self.crossposting_cache: OrderedDict[int, UserCrosspostCache] = OrderedDict()
 
+        self.crosspost_timedelta_threshold = crosspost_timedelta_threshold
         self.max_tracked_users = max_tracked_users
         self.max_tracked_message_groups_per_user = max_tracked_message_groups_per_user
         self.message_length_threshold = message_length_threshold
@@ -89,7 +92,7 @@ class AntiCrosspostCog(BaseExtensionCog, name="anti-crosspost"):
         """
         if (
             message.author.bot
-            or not self._is_valid_channel(message.channel) # type: ignore
+            or not self._is_valid_channel(message.channel)  # type: ignore
             or message.type != discord.MessageType.default
             or (
                 len(message.content) < self.message_length_threshold
@@ -98,6 +101,15 @@ class AntiCrosspostCog(BaseExtensionCog, name="anti-crosspost"):
         ):
             return
 
+        # Attempt to enforce the cache size limit
+        for user_id in list(self.crossposting_cache.keys()):
+            if len(self.crossposting_cache) <= self.max_tracked_users:
+                break
+
+            user_cache = self.crossposting_cache[user_id]
+            if not any(len(group) > 1 for group in user_cache["message_groups"]):
+                self.crossposting_cache.pop(user_id)
+
         # Initialize cache for new users
         if message.author.id not in self.crossposting_cache:
             self.crossposting_cache[message.author.id] = UserCrosspostCache(
@@ -105,50 +117,50 @@ class AntiCrosspostCog(BaseExtensionCog, name="anti-crosspost"):
                 message_to_alert={},
             )
         else:
-            # Remove oldest user if the limit is exceeded
-            if len(self.crossposting_cache) > self.max_tracked_users:
-                self.crossposting_cache.popitem(last=False)
+            user_cache = self.crossposting_cache[message.author.id]
 
-        user_cache = self.crossposting_cache[message.author.id]
+            # Check for crossposts or duplicates in existing message groups
+            for messages in user_cache["message_groups"]:
+                for existing_message in messages:
+                    if (
+                        crosspost_cmp(message, existing_message)
+                        and message.created_at.timestamp()
+                        - existing_message.created_at.timestamp()
+                        <= self.crosspost_timedelta_threshold
+                    ):
+                        messages.append(message)
 
-        # Remove old message groups if limit is exceeded and the oldest group is too small
-        if (
-            len(user_cache["message_groups"]) > self.max_tracked_message_groups_per_user
-            and len(user_cache["message_groups"][0]) < 2
-        ):
-            user_cache["message_groups"].pop(0)
-
-        # Check for crossposts or duplicates in existing message groups
-        for messages in user_cache["message_groups"]:
-            for existing_message in messages:
-                if message.channel.id != existing_message.channel.id and crosspost_cmp(
-                    message, existing_message
-                ):
-                    messages.append(message)
-
-                    # Send an alert message and add its ID to the alert list
-                    try:
-                        alert_message = await message.reply(
-                            "This message is a recent crosspost/duplicate of the following messages: "
-                            + ", ".join([m.jump_url for m in messages])
-                            + ".\n\nPlease delete all duplicate messages."
-                        )
-                        user_cache["message_to_alert"][message.id] = alert_message.id
-                    except discord.HTTPException:
-                        # Silently handle errors
-                        pass
-                    break
+                        # Send an alert message and add its ID to the alert list
+                        try:
+                            alert_message = await message.reply(
+                                "This message is a recent crosspost/duplicate among the following messages: "
+                                + ", ".join([m.jump_url for m in messages])
+                                + ".\n\nPlease delete all duplicate messages."
+                            )
+                            user_cache["message_to_alert"][
+                                message.id
+                            ] = alert_message.id
+                        except discord.HTTPException:
+                            # Silently handle errors
+                            pass
+                        break
+                else:
+                    continue
+                break
             else:
-                continue
-            break
-        else:
-            # Add new message group if no crosspost is found
-            user_cache["message_groups"].append([message])
-            if (
-                len(user_cache["message_groups"])
-                > self.max_tracked_message_groups_per_user
-            ):
-                user_cache["message_groups"].pop(0)
+                user_cache["message_groups"].append([message])
+
+                # Remove oldest message groups if limit is exceeded and the group is too small
+                if (
+                    len(user_cache["message_groups"])
+                    > self.max_tracked_message_groups_per_user
+                ):
+                    for i, messages in reversed(
+                        tuple(enumerate(user_cache["message_groups"]))
+                    ):
+                        if len(messages) < 2:
+                            user_cache["message_groups"].pop(i)
+                            break
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message):
@@ -168,7 +180,7 @@ class AntiCrosspostCog(BaseExtensionCog, name="anti-crosspost"):
         stale_alert_message_ids: list[int] = []
 
         for messages in user_cache["message_groups"]:
-            for j in reversed(range(len(messages))):
+            for j in range(len(messages) - 1, -1, -1):
                 if message.id == messages[j].id:
                     del messages[j]
                     if message.id in user_cache["message_to_alert"]:
@@ -176,9 +188,9 @@ class AntiCrosspostCog(BaseExtensionCog, name="anti-crosspost"):
                             user_cache["message_to_alert"].pop(message.id)
                         )
                     break
-
+            # Mark all alert messages for this crosspost group as stale if the group
+            # has only one message
             if len(messages) == 1:
-                # Mark all alert messages for this crosspost group as stale
                 stale_alert_message_ids.extend(user_cache["message_to_alert"].values())
                 user_cache["message_to_alert"].clear()
 
@@ -226,6 +238,7 @@ async def setup(
     channel_ids: Collection[int],
     max_tracked_users: int = 10,
     max_tracked_message_groups_per_user: int = 10,
+    crosspost_timedelta_threshold: int = 86400,
     message_length_threshold: int = 64,
     theme_color: int | discord.Color = 0,
 ):
@@ -237,6 +250,7 @@ async def setup(
         channel_ids (Collection[int]): Collection of channel IDs to monitor.
         max_tracked_users (int): Maximum number of users to track.
         max_tracked_message_groups_per_user (int): Maximum number of message groups to track per user.
+        crosspost_timedelta_threshold (int): Minimum time difference between messages to not be considered crossposts.
         message_length_threshold (int): Minimum length of a message to be considered.
         theme_color (int | discord.Color): Theme color for the bot's responses.
     """
@@ -244,6 +258,7 @@ async def setup(
         AntiCrosspostCog(
             bot,
             channel_ids,
+            crosspost_timedelta_threshold,
             message_length_threshold,
             max_tracked_users,
             max_tracked_message_groups_per_user,
